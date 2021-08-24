ba60d5d0772fe03cc9c2c36b76e73ae58334d187
Julian Martinez <julianmartinez1830@gmail.com>
2021-08-24 11:30:14 -0500

    feat: TDD completed setup and intro

--- code.js
+++ code.js
@@ -1,4 +1,14 @@
-// helloWorld function 
-function helloWorld() {
+// helloWorld function
+//  function statement:
+// function helloWorld() {
+//     return "Hello, World!";
+// }
+//  function expression:
+const helloWorld = function(){
     return "Hello, World!";
+}
+
+//  sayHello function
+function sayHello(){
+    return "Hello!";
 }
