892aee0f73468a83f39c4ccaf39e34f083b6e0bd
Julian Martinez <julianmartinez1830@gmail.com>
2021-08-24 12:26:19 -0500

    feat: refactored sayHello()

--- code.js
+++ code.js
@@ -10,11 +10,13 @@ const helloWorld = function(){
 
  // sayHello function
 function sayHello(input){
-    if(input === 'Alex'){
-        return "Hello, Alex!";
-    }else if(input === 'Pat'){
-        return "Hello, Pat!";
-    }else{
-        return "Hello, Jane!";
-    }
+    // if(input === 'Alex'){
+    //     return "Hello, Alex!";
+    // }else if(input === 'Pat'){
+    //     return "Hello, Pat!";
+    // }else{
+    //     return "Hello, Jane!";
+    // }
+    //  refactored
+    return "Hello, " + input + "!";
 }
