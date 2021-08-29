64a1e53078707c2c35b19e175992a330d72edbba
Julian Martinez <julianmartinez1830@gmail.com>
2021-08-29 11:28:13 -0500

    feat: completed spec2 for TDD isVowel

--- code.js
+++ code.js
@@ -8,54 +8,54 @@ const helloWorld = function(){
     return "Hello, World!";
 }
 
- // sayHello function
-function sayHello(input){
-    if(input === true){
-        return "Hello, World!";
-    }
-    if(input === false){
-        return "Hello, World!";
-    }
-    if(typeof input === 'undefined'){
-        return "Hello, World!";
-    }else if(input === null){
-        return "Hello, World!";
-    }else if(input === ''){
-        return "I'm an empty string!";
-    }else if(typeof input === 'number'){
-        return "I'm a number!";
-    }else if(input === 'Jane' || input === 'Alex' || input === 'Pat'){
-        return `Hello, ${input}!`;
-    }else{
-        return 'I\'m a string!';
-    }
+//  // sayHello function
+// function sayHello(input){
+//     if(input === true){
+//         return "Hello, World!";
+//     }
+//     if(input === false){
+//         return "Hello, World!";
+//     }
+//     if(typeof input === 'undefined'){
+//         return "Hello, World!";
+//     }else if(input === null){
+//         return "Hello, World!";
+//     }else if(input === ''){
+//         return "I'm an empty string!";
+//     }else if(typeof input === 'number'){
+//         return "I'm a number!";
+//     }else if(input === 'Jane' || input === 'Alex' || input === 'Pat'){
+//         return `Hello, ${input}!`;
+//     }else{
+//         return 'I\'m a string!';
+//     }
+//
+//    //  REFACTORED
+//     // if(typeof input === "boolean" || typeof input === "undefined" || input === null){
+//     //     return helloWorld;
+//     // }else{
+//     //     return "Hello, " + input + "!";
+//     // }
+//     //  EDGE CASES
+// }
 
-   //  REFACTORED
-    // if(typeof input === "boolean" || typeof input === "undefined" || input === null){
-    //     return helloWorld;
-    // }else{
-    //     return "Hello, " + input + "!";
-    // }
-    //  EDGE CASES
-}
-
-function isFive(input){
-    // if(input == '5'){
-    //     return true;
-    // }else if(typeof input === 'string'){
-    //     return false;
-    // }else if(input !== 5){
-    //     return false
-    // }else {
-    //     return true;
-    // }
-    //  REFACTORED
-    if(input == 5){
-        return true;
-    }else{
-        return false;
-    }
-}
+// function isFive(input){
+//     // if(input == '5'){
+//     //     return true;
+//     // }else if(typeof input === 'string'){
+//     //     return false;
+//     // }else if(input !== 5){
+//     //     return false
+//     // }else {
+//     //     return true;
+//     // }
+//     //  REFACTORED
+//     if(input == 5){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
 
 function isEven(input){
     //  REFACTOR
@@ -64,4 +64,12 @@ function isEven(input){
     }else{
         return false;
     }
+}
+
+//  isVowel Function
+function isVowel(input){
+    if(input === 'a'){
+        return true;
+    }
+    return typeof input === 'boolean';
 }
